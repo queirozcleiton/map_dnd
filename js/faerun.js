37b185367d96fc71fc5000273f0ed07020b0651b
queirozcleiton <queiroz.cleiton@gmail.com>
2026-03-24 08:25:10 -0300

    Update faerun.js

--- js/faerun.js
+++ js/faerun.js
@@ -1,7 +1,7 @@
 initMap({
   divId: "map",
   image: "assets/forgotten-realms.png",
-  width: 2797,   // ajuste quando tiver o mapa
-  height: 4096,  // ajuste quando tiver o mapa
+  width: 4096,   // ajuste quando tiver o mapa
+  height: 2797,  // ajuste quando tiver o mapa
   data: "data/faerun.json"
 });
